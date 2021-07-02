14e5d690e3e7ee04e41987e7c679b8bc1c483ce3
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-02 16:51:20 -0400

    adjust home screen and game board

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # monopoly
+I. Pseudocode:
+
+II. 
+
+III.References:
+1. Custom-Select-Box: https://github.com/Godsont/Custom-Select-Box
+2. 
--- js/app.js
+++ js/app.js
@@ -1,15 +1,36 @@
 /*-------------------------------- Constants --------------------------------*/
-const initialFund, plCount, player1, player2, player3, player4;
+// const plCount, player1, player2, player3, player4;
+
+const initialFund = 2000
 
 /*---------------------------- Variables (state) ----------------------------*/
 let player1fund, player2fund, player3fund, player4fund, player1plot, player2plot, player3plot, player4plot, turn, winner; 
 
 
 /*------------------------ Cached Element References ------------------------*/
+const boardEl = document.getElementById('board');
+const playBtn = document.getElementById('playGame');
+// const playBtn = document.getElementById('playGame');
 
+// Home selction screen
+const selected = document.querySelector(".selected");
+const optContainer = document.querySelector(".options-container");
+const optionsList = document.querySelectorAll(".option");
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+selected.addEventListener("click", () => {
+    optContainer.classList.toggle("active");
+    console.log('hi')
+  });
+  
+optionsList.forEach(opt => {
+    opt.addEventListener("click", () => {
+      selected.innerHTML = opt.querySelector("label").innerHTML;
+      optContainer.classList.remove("active");
+    });
+});
 
 
 
