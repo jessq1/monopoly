e2a1fa67eee8a1bbb361d589c2054de41b30a78d
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-02 13:23:55 -0400

    establish variables

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const initialFund, plCount, player1, player2, player3, player4;
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let player1fund, player2fund, player3fund, player4fund, player1plot, player2plot, player3plot, player4plot, turn, winner; 
 
 
 /*------------------------ Cached Element References ------------------------*/
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
