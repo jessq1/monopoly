04e8c7d6be7d922fdd036366103d77d4aad4fc81
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-04 10:46:41 -0400

    add init function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 /*-------------------------------- Constants --------------------------------*/
-// const plCount, player1, player2, player3, player4;
 
 const initialFund = 2000 
-// const plCount
+const player1 = "Player 1"
+const player2 = "Player 2"
+const player3 = "Player 3"
+const player4 = "Player 4"
+
 
 /*---------------------------- Variables (state) ----------------------------*/
 let plCount, player1fund, player2fund, player3fund, player4fund, player1plot, player2plot, player3plot, player4plot, turn, winner; 
@@ -31,25 +34,39 @@ PlayerNumSel.addEventListener("click", (evt) => {
 playBtn.addEventListener("click", () => {
     homePage.style.display = 'none'
     gamePage.style.display = 'grid'
+    init()
 })
 
 homeBtn.addEventListener("click", ()=>{
   homePage.style.display = 'flex'
   gamePage.style.display = 'none'
   playBtn.style.display = "none"
-
+  init()
 })
-// optionsList.forEach(opt => {
-//     opt.addEventListener("click", () => {
-//       selected.innerHTML = opt.querySelector("label").innerHTML;
-//       optContainer.classList.remove("active");
-//     });
-// });
 
 
 
 /*--------------------------------- Objects ---------------------------------*/
+// 40 plots as objects:
+
+// chances cards as objects:
+
+// dice as objects:
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function init(){
+  player1fund = initialFund;
+  player2fund = initialFund;
+  player3fund = initialFund;
+  player4fund = initialFund;
+  player1plot = [];
+  player2plot = [];
+  player3plot = [];
+  player4plot = [];
+  turn = null;
+  winner = null;
+
+}
