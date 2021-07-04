ddd8b8990aed8fdb7643ef174c4afe0ee57bdb6c
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-04 10:30:21 -0400

    adjust home screen transition to gamepage

--- js/app.js
+++ js/app.js
@@ -1,36 +1,48 @@
 /*-------------------------------- Constants --------------------------------*/
 // const plCount, player1, player2, player3, player4;
 
-const initialFund = 2000
+const initialFund = 2000 
+// const plCount
 
 /*---------------------------- Variables (state) ----------------------------*/
-let player1fund, player2fund, player3fund, player4fund, player1plot, player2plot, player3plot, player4plot, turn, winner; 
+let plCount, player1fund, player2fund, player3fund, player4fund, player1plot, player2plot, player3plot, player4plot, turn, winner; 
 
 
 /*------------------------ Cached Element References ------------------------*/
 const boardEl = document.getElementById('board');
 const playBtn = document.getElementById('playGame');
-// const playBtn = document.getElementById('playGame');
+const homeBtn = document.getElementById('backToHome');
+const homePage = document.querySelector("div.home");
+const gamePage = document.querySelector(".gamePage");
 
 // Home selction screen
-const selected = document.querySelector(".selected");
-const optContainer = document.querySelector(".options-container");
-const optionsList = document.querySelectorAll(".option");
+const PlayerNumSel = document.querySelector(".playerNumSelection");
+
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-selected.addEventListener("click", () => {
-    optContainer.classList.toggle("active");
-    console.log('hi')
+
+PlayerNumSel.addEventListener("click", (evt) => {
+    plCount = evt.target.id.charAt(0)
   });
   
-optionsList.forEach(opt => {
-    opt.addEventListener("click", () => {
-      selected.innerHTML = opt.querySelector("label").innerHTML;
-      optContainer.classList.remove("active");
-    });
-});
+playBtn.addEventListener("click", () => {
+    console.log(homePage)
+    homePage.style.display = 'none'
+    gamePage.style.display = 'grid'
+})
+
+homeBtn.addEventListener("click", ()=>{
+  homePage.style.display = 'flex'
+  gamePage.style.display = 'none'
+})
+// optionsList.forEach(opt => {
+//     opt.addEventListener("click", () => {
+//       selected.innerHTML = opt.querySelector("label").innerHTML;
+//       optContainer.classList.remove("active");
+//     });
+// });
 
 
 
